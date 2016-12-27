d131e5e3f3114c2ebf996eea674fce97c858fce8
Douglas Waugh <douglas.waugh.e3@gmail.com>
2016-12-27 19:06:15 +0000

    Sketch out UI

--- spec/subs_spec.rb
+++ spec/subs_spec.rb
@@ -1,8 +1,26 @@
 require "spec_helper"
 
-describe Subs do
+# Adding court booking (write event)
+# add-court-booking --player jamie --cost 18 --date 5/1/2016
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
+# Adding a new practice (write events for each player)
+# add-practice --players douglas,jamie,raul --balls douglas,jamie --date 15/12/2016
+
+# Adding payment received
+# add-payment-received --from jamie --amount 50 --type cash/coolhurst-transfer/bank-transfer (--reference?)
+
+# Adding payment made
+# add-payment-made --to jamie --amount 50 --type cash/coolhurst-transfer/bank-transfer
+
+# Print balance for player (load events, filter by player, hydrate player, print statement - get to define everything we need)
+#   court fee event
+#   balls fee event
+#   transfer received event
+#   cash received event
+#   booked court event
+#   provided balls event
+#   transfer paid event
+# get-balance --player jamie
+
+# Print balance for all players (load events, hydrate all players, print statement for each player)
+# get-balances
