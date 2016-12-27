1f9d7c4bcfaed066451939952e3217530fb15106
Douglas Waugh <douglas.waugh.e3@gmail.com>
2016-12-27 19:22:34 +0000

    Add Player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+
+describe Player do
+
+  it "should create a file for a new practice" do
+    expect(false).to eq(false)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "subs"
+require "player"
