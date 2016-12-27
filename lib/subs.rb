6f3aa55fa6a83175e21466de7677e43c225cc842
Douglas Waugh <douglas.waugh.e3@gmail.com>
2016-12-27 19:04:19 +0000

    Change extension of subs file and add options to add-practice

---
+++ lib/subs.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'thor'
+
+class Subs < Thor
+  desc "add-practice", "add a new practice" 
+  method_option :players,:type => :array,:desc => "People who played"
+  method_option :cost,:type => :numeric,:desc => "Cost of court booking"
+  method_option :balls,:type => :array,:desc => "People who brought the balls"
+  def add_practice
+    if options[:players]
+      puts "Players"
+      options[:players].each { |player| puts "  #{player}" }  
+    end
+    if options[:cost]
+      puts "Cost"
+      puts "  #{options[:cost]}" if options[:cost]
+    end
+    if options[:balls]
+      puts "Balls"
+      options[:balls].each { |player| puts "  #{player}" }
+    end
+  end
+
+  def 
+end
+
+Subs.start(ARGV)
