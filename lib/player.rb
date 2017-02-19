97be9cf9a6b2867ca78abecc0c32c28f038fb21b
Douglas Waugh <douglas.waugh.e3@gmail.com>
2017-02-19 16:56:07 +0000

    Remove duplicated event handling methods from Player

--- lib/player.rb
+++ lib/player.rb
@@ -13,30 +13,6 @@ class Player
   end
 
   def handle_event(event)
-    send(event[:type], event)
-  end
-
-  def transfer_received(event)
-    @balance = @balance + event[:amount]
-  end
-
-  def transfer_sent(event)
-    @balance = @balance + event[:amount]
-  end
-
-  def participated_in_practice(event)
-    @balance = @balance + event[:amount]
-  end
-
-  def provided_balls(event)
-    @balance = @balance + event[:amount]
-  end
-
-  def booked_court(event)
-    @balance = @balance + event[:amount]
-  end
-
-  def paid(event)
     @balance = @balance + event[:amount]
   end
 
