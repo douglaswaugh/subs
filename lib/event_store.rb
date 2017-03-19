64c33b749ea31788d0f0a960bae101a8d9633704
Douglas Waugh <douglas.waugh.e3@gmail.com>
2017-03-19 19:18:56 +0000

    Use two-space tabs instead of four-space in EventStore

--- lib/event_store.rb
+++ lib/event_store.rb
@@ -1,11 +1,11 @@
 class EventStore
-    def load_events
-        events = []
-        
-        Dir["data/*.yml"].each do |filename| 
-            events << YAML.load_file(filename)
-        end
+  def load_events
+    events = []
 
-        return events
+    Dir["data/*.yml"].each do |filename| 
+      events << YAML.load_file(filename)
     end
+
+    return events
+  end
 end
