3cab824db2664a58fa94fa053f8014f5227bceb8
Douglas Waugh <douglas.waugh.e3@gmail.com>
2017-07-09 12:54:47 +0100

    Add tag and command line output to test

--- spec/check_players_balances_spec.rb
+++ spec/check_players_balances_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
-describe PlayerRepository do
+describe PlayerRepository, :validate_data => true do
   it 'should load all the players and their balances should be correct' do
     playerRepository = PlayerRepository.new(EventStore.new, PlayerIds.new(YamlLoader.new))
 
     playerRepository.get_players().each do |player|
+      puts "Processing #{player}"
       playerRepository.get_player(player[0])
     end
   end
