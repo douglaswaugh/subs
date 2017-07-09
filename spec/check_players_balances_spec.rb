e73ddedf0daa7558f49dc2703dc3220eef51881a
Douglas Waugh <douglas.waugh.e3@gmail.com>
2017-07-09 12:28:06 +0100

    Check all players load without errors

--- spec/check_players_balances_spec.rb
+++ spec/check_players_balances_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PlayerRepository do
   it 'should load all the players and their balances should be correct' do
-    
+    playerRepository = PlayerRepository.new(EventStore.new, PlayerIds.new(YamlLoader.new))
+
+    playerRepository.get_players().each do |player|
+      playerRepository.get_player(player[0])
+    end
   end
 end
